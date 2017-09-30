9ca037b1386d6a969dade6a4465344d0f0620b06
XINPANG\X.Pang <joyce.xin.pang@gmail.com>
2017-09-30 16:04:25 +0200

    Exploratory stats.

---
+++ scripts/feature_engg.py
@@ -0,0 +1,7 @@
+
+
+def label_attendance(attend):
+    if attend == 'yes' or attend == 'waitlist':
+        return 1
+    else:
+        return 0
---
+++ scripts/feature_util.py
@@ -0,0 +1,37 @@
+import json
+import pandas as pd
+
+
+def flatten_dict(df, col):
+    pd_new = pd.concat([df.drop(col, axis=1), df[col].apply(pd.Series)], axis=1)
+    return pd_new
+
+
+def flatten_list(df, col, reset_index=False):
+    col_flat = pd.DataFrame([[i, x] 
+                       for i, y in df[col].apply(list).iteritems() 
+                           for x in y], columns=['I', col])
+    col_flat = col_flat.set_index('I')
+    df = df.drop(col, 1)
+    df = df.merge(col_flat, left_index=True, right_index=True)
+    if reset_index:
+        df = df.reset_index(drop=True)
+    return df
+
+
+def get_df(path, encoding):
+    i = 0
+    df = {}
+    for d in parse(path, encoding):
+        df[i] = d
+        i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
+
+
+def parse(path, encoding):
+
+    with open(path) as data_file:
+        data = json.load(data_file, encoding=encoding)
+    print type(data)
+    return data
+
