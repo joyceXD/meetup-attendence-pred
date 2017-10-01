cd59313bddaef110045cb4b39570dca8d65d4478
Pang, Xin <xin.pang@philips.com>
2017-10-01 12:02:01 +0200

    Initial draft model.

--- scripts/feature_engg.py
+++ scripts/feature_engg.py
@@ -1,7 +1,19 @@
 
 
+def event_hour_transform(h_value):
+    value = math.cos(2*math.pi*(h_value-6)/24)
+    return value
+
+
 def label_attendance(attend):
     if attend == 'yes' or attend == 'waitlist':
         return 1
     else:
         return 0
+
+
+def lat_lon_similarity(lat1, lon1, lat2, lon2):
+    lat_diff = lat1-lat2
+    lon_diff = lon1-lon2
+    sim = (lat_diff**2+lon_diff**2)**(0.5)
+    return sim
